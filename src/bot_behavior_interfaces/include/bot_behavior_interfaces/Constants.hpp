2f534982ff6c98ea755d4cb7a4a92e6e1eb63343
ronyk <rony.kaddoum.2013@gmail.com>
2024-08-24 03:18:34 +0300

    Create custom interfaces and constants

---
+++ src/bot_behavior_interfaces/include/bot_behavior_interfaces/Constants.hpp
@@ -0,0 +1,12 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+#include <cstdint>
+namespace item_position
+{
+    constexpr uint8_t NOT_IN_VIEW = 0;
+    constexpr uint8_t LEFT = 1;
+    constexpr uint8_t CENTER = 2;
+    constexpr uint8_t RIGHT = 3;
+}
+
+#endif // TURN_CONSTANTS_HPP
