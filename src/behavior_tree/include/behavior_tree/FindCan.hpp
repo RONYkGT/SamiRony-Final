0c32aabc4bd8fef45a5e2e501573d901372e43e9
ronyk <rony.kaddoum.2013@gmail.com>
2024-08-25 04:00:38 +0300

    Change FindCan to stateful action node

--- src/behavior_tree/include/behavior_tree/FindCan.hpp
+++ src/behavior_tree/include/behavior_tree/FindCan.hpp
@@ -1,36 +1,44 @@
 #ifndef FIND_CAN_HPP
 #define FIND_CAN_HPP
 
-#include <behaviortree_cpp_v3/action_node.h>
+#include <string>
+#include <chrono>
+#include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
 #include <std_msgs/msg/u_int8.hpp>
-#include <rclcpp/rclcpp.hpp>
+#include <behaviortree_cpp_v3/action_node.h>
+#include "bot_behavior_interfaces/Constants.hpp" 
 #include "bot_behavior_interfaces/action/turn_left.hpp"
 #include "bot_behavior_interfaces/action/turn_right.hpp"
-#include "bot_behavior_interfaces/Constants.hpp" 
 
-class FindCan : public BT::ActionNodeBase
+class FindCan : public BT::StatefulActionNode
 {
 public:
     FindCan(const std::string &name, const BT::NodeConfiguration &config);
 
-    BT::NodeStatus tick() override;
-    void halt() override;
+    // The overridden methods for the stateful action node
+    BT::NodeStatus onStart() override;
+    BT::NodeStatus onRunning() override;
+    void onHalted() override;
 
     static BT::PortsList providedPorts() {
         return {};
     }
 
-private:
+    // Callback function for the subscription
     void callback(const std_msgs::msg::UInt8::SharedPtr msg);
+
+private:
     rclcpp::Node::SharedPtr node_;
     rclcpp::Subscription<std_msgs::msg::UInt8>::SharedPtr subscriber_;
     rclcpp_action::Client<bot_behavior_interfaces::action::TurnLeft>::SharedPtr turn_left_client_;
     rclcpp_action::Client<bot_behavior_interfaces::action::TurnRight>::SharedPtr turn_right_client_;
+
     rclcpp_action::ClientGoalHandle<bot_behavior_interfaces::action::TurnLeft>::SharedPtr turn_left_goal_handle_;
     rclcpp_action::ClientGoalHandle<bot_behavior_interfaces::action::TurnRight>::SharedPtr turn_right_goal_handle_;
-    bool turning_in_progress_ = false;
-    uint8_t can_position_ = item_position::NOT_IN_VIEW;  // 0=NOT_IN_VIEW, 1=LEFT, 2=CENTER, 3=RIGHT
+
+    bool turning_in_progress_;
+    uint8_t can_position_;
 };
 
 #endif // FIND_CAN_HPP
