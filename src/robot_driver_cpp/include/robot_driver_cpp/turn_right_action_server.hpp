34a6b580bf5f7368193a64ae32df290925e67f69
ronyk <rony.kaddoum.2013@gmail.com>
2024-08-26 04:59:52 +0300

    Convert robot_driver to cpp

---
+++ src/robot_driver_cpp/include/robot_driver_cpp/turn_right_action_server.hpp
@@ -0,0 +1,42 @@
+#ifndef TURN_RIGHT_ACTION_SERVER_HPP
+#define TURN_RIGHT_ACTION_SERVER_HPP
+
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "bot_behavior_interfaces/action/turn_right.hpp"
+
+class TurnRightActionServer : public rclcpp::Node
+{
+public:
+    using TurnRight = bot_behavior_interfaces::action::TurnRight;
+    using GoalHandleTurnRight = rclcpp_action::ServerGoalHandle<TurnRight>;
+
+    TurnRightActionServer();
+
+private:
+    rclcpp_action::Server<TurnRight>::SharedPtr action_server_;
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    double turn_speed_;
+    geometry_msgs::msg::Twist twist_msg_;
+
+    rclcpp_action::GoalResponse handle_goal(
+        const rclcpp_action::GoalUUID &uuid,
+        std::shared_ptr<const TurnRight::Goal> goal
+    );
+
+    rclcpp_action::CancelResponse handle_cancel(
+        const std::shared_ptr<GoalHandleTurnRight> goal_handle
+    );
+
+    void handle_accepted(const std::shared_ptr<GoalHandleTurnRight> goal_handle);
+
+    void execute(const std::shared_ptr<GoalHandleTurnRight> goal_handle);
+
+    void publish_twist_msg();
+
+    void stop_robot();
+};
+
+#endif // TURN_RIGHT_ACTION_SERVER_HPP
