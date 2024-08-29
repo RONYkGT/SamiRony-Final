bf73437cfa2135182a8547533e6c30205d3f5c16
Samitrad <samitrad7@gmail.com>
2024-08-29 03:20:34 +0300

    Add config file for robot_driver_cpp

--- src/robot_driver_cpp/launch/robot_driver.launch.py
+++ src/robot_driver_cpp/launch/robot_driver.launch.py
@@ -1,15 +1,24 @@
-from launch import LaunchDescription
-from launch_ros.actions import Node
+import launch
+import launch_ros.actions
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
 
 def generate_launch_description():
-    # Launch the turn_action_servers executable
-    turn_action_servers_node = Node(
-        package='robot_driver_cpp',
-        executable='turn_action_servers',
-        name='turn_action_servers',
-        output='screen'
+    # Define the path to the config file for the robot_driver_cpp package
+    config_file = os.path.join(
+        get_package_share_directory('robot_driver_cpp'), 'config', 'config.yaml'
     )
 
-    return LaunchDescription([
-        turn_action_servers_node
+    return launch.LaunchDescription([
+        launch_ros.actions.Node(
+            package='robot_driver_cpp',
+            executable='turn_action_servers',
+            name='turn_action_servers',
+            output='screen',
+            parameters=[config_file]
+        ),
+        
+        
     ])
